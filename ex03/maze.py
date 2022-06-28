aa10c1e3c9e4ca3d9d5b32379d475b6044820344
ryoga <c0a211465d@edu.teu.ac.jp>
2022-06-28 14:20:29 +0900

    迷路１

---
+++ ex03/maze.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.geometry("1500x900")
+    root.configure(bg="black")
+    root.title("迷えるこうかとん")
+    #label = tk.Label(root,text = "hello",font=("Time Ner Roman", 80))
+    #label.pack()
+    root.mainloop()
