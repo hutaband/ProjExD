b3b25cd198c75b878c6f39982c3e55cbe2d173da
ryoga <c0a211465d@edu.teu.ac.jp>
2022-06-28 17:10:53 +0900

    maze.py
    git commit -m maze.py

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,4 +1,6 @@
+import math
 import tkinter as tk
+from tkinter import font
 import maze_maker as mm
 
 def key_down(event):
@@ -11,7 +13,8 @@ def key_up(event):
     key = ""
 
 def main_proc():
-    global mx,my,cx,cy
+    global mx,my,cx,cy,time
+    time = time-0.1           #１秒ずつ減る
     #delta = {"Up"   : [0,-20],
     #         "Down" : [0,+20],
     #         "Left" : [-20,0],
@@ -33,9 +36,14 @@ def main_proc():
 
     cx, cy = mx*100+50,my*100+50  
     canvas.coords("tori",cx,cy) 
+    label_2["text"]=math.floor(time)
+    if label_2["text"] == 0:          #制限時間
+        time = 0
     root.after(100,main_proc)
 
+
 if __name__ == "__main__":
+    time = 60
     root = tk.Tk()
     #root.geometry("1500x900")
     #root.configure(bg="black")
@@ -46,7 +54,11 @@ if __name__ == "__main__":
 
     maze_bg = mm.make_maze(15,9)
     mm.show_maze(canvas,maze_bg)
-    
+    label_1 = tk.Label(root,text="スタート")   #スタート位置
+    label_1.place(x = 100,y = 100)
+    label_2 = tk.Label(root,text = time,font=("MSゴシック",30))   #時間表示
+    label_2.place(x = 750,y = 50)
+    label_3 = tk.Label(root,text="GAMEOVER")    #ゲームオーバー
     tori = tk.PhotoImage(file="fig/3.png")
     mx, my = 1,1
     cx,cy = mx*100+50,my*100+50
