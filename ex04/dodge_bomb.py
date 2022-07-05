52cfcf9b3d63dbbe446f307a36c3a28634374b7f
ryoga <c0a211465d@edu.teu.ac.jp>
2022-07-05 14:48:44 +0900

    練習5

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,5 +1,6 @@
 import pygame as pg
 import sys
+import random
 
 def main():
     clock = pg.time.Clock()
@@ -16,6 +17,14 @@ def main():
     kkimg_sfc = pg.transform.rotozoom(kkimg_sfc, 0, 2.0) #Surface
     kkimg_rct = kkimg_sfc.get_rect() #Rect
     kkimg_rct.center = 900,400
+
+    #練習5
+    bmimg_sfc = pg.Surface((20, 20)) #Surface
+    bmimg_sfc.set_colorkey((0,0,0))
+    pg.draw.circle(bmimg_sfc, (255,0,0), (10,10), 10)
+    bmimg_rct = bmimg_sfc.get_rect() #Rect
+    bmimg_rct.centerx = random.randint(0, screen_rct.width)
+    bmimg_rct.centery = random.randint(0, screen_rct.height)
 
 
 
@@ -38,6 +47,9 @@ def main():
         if key_states[pg.K_RIGHT] == True: #x座標を+1
             kkimg_rct.centerx += 1
         screen_sfc.blit(kkimg_sfc,kkimg_rct)
+
+        #練習5
+        screen_sfc.blit(bmimg_sfc,bmimg_rct)
             
 
         pg.display.update()
