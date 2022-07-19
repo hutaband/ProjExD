9b2802556164e8cf630accae99b9b0c0368377fb
ryoga <c0a211465d@edu.teu.ac.jp>
2022-07-19 18:14:23 +0900

    コメント追加

--- ex06/squash.py
+++ ex06/squash.py
@@ -4,7 +4,7 @@ import pygame as pg
 
 
 class Screen:
-    def __init__(self, title, wh, image):
+    def __init__(self, title, wh, image):  #初期メソッド
         pg.display.set_caption(title)
         self.sfc = pg.display.set_mode(wh)     # Surface
         self.rct = self.sfc.get_rect()         # Rect
@@ -43,7 +43,7 @@ class Racket:
         self.blit(scr)
 
 
-class Ball:
+class Ball:  #ボール生成
     def __init__(self, fname, rack):
         self.image = pg.image.load(fname).convert_alpha()
         self.image = pg.transform.scale(self.image,(50,50))
@@ -52,7 +52,7 @@ class Ball:
         self.v_y = 1
         self.racket = rack
 
-    def ball_move(self, scr: Screen):
+    def ball_move(self, scr: Screen): #ボールの挙動
         self.rct.centerx += int(self.v_x)
         self.rct.centery += int(self.v_y)
 
@@ -66,7 +66,7 @@ class Ball:
             self.rct.top = scr.rct.top
             self.v_y = -self.v_y
 
-        if self.rct.colliderect(self.racket.rct):
+        if self.rct.colliderect(self.racket.rct): #ボールとバーの衝突
             dist = self.rct.centerx - self.racket.rct.centerx
             if dist < 0:
                 self.v_x = -1
@@ -76,28 +76,28 @@ class Ball:
                 self.v_x = random.randint(-5,5)
             self.v_y = -1
 
-        if self.rct.bottom > scr.rct.bottom:
+        if self.rct.bottom > scr.rct.bottom: #ボールが画面の下に行った場合
             font = pg.font.Font(None, 100)
             text = font.render("GAME OVER", True,(255,0,0))
             scr.text_blit(text, 400,200)
 
 
-    def draw(self, sfc):
+    def draw(self, sfc): #ボールの描画
         sfc.blit(self.image, self.rct)
 
 
-    def update(self,scr:Screen):
+    def update(self,scr:Screen): #更新
         scr.sfc.blit(self.image,self.rct)
 
-def main():
+def main(): #メイン関数
     clock = pg.time.Clock()
-    scr = Screen("squash", (1200, 600), "fig/haikei.png")
-    rack = Racket("fig/beam.png",0.08,(550,550))
-    ball = Ball("fig/boss.png", rack)
+    scr = Screen("squash", (1200, 600), "fig/haikei.png") #スクリーン設定
+    rack = Racket("fig/beam.png",0.08,(550,550)) #バーの設定
+    ball = Ball("fig/boss.png", rack) #ボールの設定
 
     while True:
         scr.blit()
-        for event in pg.event.get():
+        for event in pg.event.get(): #×ボタンで終了
             if event.type == pg.QUIT: return
         
         ball.ball_move(scr)
@@ -106,13 +106,13 @@ def main():
         pg.display.update()
         clock.tick(1000)
 
-def check_bound(rct, scr_rct):
+def check_bound(rct, scr_rct): #バーと壁の判定
     yoko, tate = +1, +1 # 領域内
     if rct.left < scr_rct.left or scr_rct.right  < rct.right : yoko = -1 # 領域外
     if rct.top  < scr_rct.top  or scr_rct.bottom < rct.bottom: tate = -1 # 領域外
     return yoko, tate
 
-if __name__ == "__main__":
+if __name__ == "__main__": #関数の呼び出し
     pg.init()
     main()
     pg.quit()
